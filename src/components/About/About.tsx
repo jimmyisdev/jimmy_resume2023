d80a3b8ad7600a41638164a606531ac9c326007f
jimmy <mac@macdeMacBook-Air.local>
2023-10-20 09:49:13 +0900

    update about text

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,33 +1,19 @@
 import styles from "./About.module.scss";
-import { educationData, proficiencyData } from "./aboutData";
+import { educationData, introText, proficiencyData } from "./aboutData";
 
 const About = () => {
   return (
     <div className={`page ${styles.about}`}>
       <section className={`sect ${styles.about_text}`}>
         <h1 className={`sect_title`}>About me</h1>
-        <p>
-          Hello! My name is Jimmy, and I am a junior web developer with a
-          passion for learning and exploring new technologies. I had been
-          studied in Taipei, Heidelberg and Kyoto. After completing my
-          master&#39;s degree in Germany, I began teaching myself how to code,
-          with a particular focus on web development.
-        </p>
-        <br />
-        <p>
-          I started my professional career as a frontend developer at a fin-tech
-          company in Taipei, where I was responsible for maintaining the
-          existing platforms, debugging, and developing new features using
-          React.js. Currently, I am expanding my skill set by learning
-          Express.js and MongoDB, with the ultimate goal of becoming a
-          proficient MERN stack developer.
-        </p>
-        <br />
-        <p>
-          Overall, I am enthusiastic about the possibilities that web
-          development offers and am always eager to take on new challenges to
-          improve my skills and knowledge.
-        </p>
+        {introText.map(item => {
+          return <>
+            <p>
+              {item}
+            </p>
+            <br />
+          </>
+        })}
       </section>
       <section className={`sect`}>
         <h1 className={`sect_title`}>Education</h1>
--- src/components/About/aboutData.ts
+++ src/components/About/aboutData.ts
@@ -1,3 +1,9 @@
+export const introText = [
+  "Hi! My name is Jimmy, and I am a web developer with a passion for learning and exploring new technologies. I had been  studied in Taipei, Heidelberg and Kyoto. After completing my master's degree in Germany, I began teaching myself how to code, with a particular focus on web development.",
+  "I began my professional journey as a frontend developer at a fin-tech company in Taipei. My role involved maintaining existing platforms, fixing issues, and creating new features using React.js. Additionally, I gained experience in building full-stack projects with Express.js, MongoDB, and Next.js. Currently, I'm in the early stages of broadening my skill set by exploring web 3-related knowledge. I have gained a basic understanding of blockchain concepts, the Ethereum system, and the syntax of Solidity.",
+  'Overall, I am enthusiastic about the possibilities that web development offers and am always eager to take on new challenges to improve my skills and knowledge.'
+]
+
 export const educationData = [
   "Fu Jen Catholic University (Taiwan) -  B.A.",
   "Heidelberg University (Germany) - M.A.",
